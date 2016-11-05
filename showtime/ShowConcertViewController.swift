09e228bea322761574160bcd8fa3116012b9cb67
Iñigo Solano <isolanop@uoc.edu>
2016-11-05 13:47:09 +0100

    Add ShowConcertViewController with (very) rough layaout

---
+++ showtime/ShowConcertViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ShowConcertViewController.swift
+//  showtime
+//
+//  Created by Iñigo Solano Pàez on 05/11/2016.
+//  Copyright © 2016 UOC. All rights reserved.
+//
+
+import UIKit
+
+class ShowConcertViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var venueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+
+    // MARK: Model
+    var concert: Concert!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
