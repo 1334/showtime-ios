83ed8a0694f234c7c843daec0c59482b1a00e5c5
Iñigo Solano <isolanop@uoc.edu>
2017-01-08 13:54:15 +0100

    split core data related protocols to separate files

--- showtime/ManagedObjectType.swift
+++ showtime/ManagedObjectType.swift
@@ -1,5 +1,5 @@
 //
-//  CoreDataProtocols.swift
+//  ManagedObjectType.swift
 //  showtime
 //
 //  Created by Iñigo Solano Pàez on 12/11/2016.
@@ -39,27 +39,5 @@ extension ManagedObjectType where Self: NSManagedObject {
         let request = Self.sortedFetchRequest
         request.predicate = predicate
         return (try? CoreDataStack.viewContext.fetch(request)) ?? []
-    }
-}
-
-protocol NamedManagedObjectType : ManagedObjectType {
-    var name: String { get set }
-}
-
-extension NamedManagedObjectType where Self : NSManagedObject {
-    static func named(_ name: String, context: NSManagedObjectContext = CoreDataStack.viewContext) -> Self {
-        let predicate = NSPredicate(format: "name =[cd] %@", name)
-        let result = Self.find(predicate: predicate).first
-        if let result = result {
-            return result
-        } else {
-            var newItem = Self(context: context)
-            newItem.name = name
-            return newItem
-        }
-    }
-    
-    static var defaultSortDescriptors: [NSSortDescriptor] {
-        return [NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.localizedCaseInsensitiveCompare))]
     }
 }
