d95b439564342408eee0f3e1f72e58acd1e3619b
Iñigo Solano <isolanop@uoc.edu>
2017-01-08 17:51:48 +0100

    double tap the map on the view tab zoom extends to display all annotations

--- showtime/ListVenuesViewController.swift
+++ showtime/ListVenuesViewController.swift
@@ -9,23 +9,20 @@
 import UIKit
 import MapKit
 
-class ListVenuesViewController: ShowtimeBaseViewController, MKMapViewDelegate {
+class ListVenuesViewController: ShowtimeBaseViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
     @IBOutlet weak var map: MKMapView!
-    var venues: [Venue]!
+    var venues = Venue.all()
     var didSelect: (Venue) -> () = { _ in }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        map.delegate = self
-        map.isPitchEnabled = false
-        map.isRotateEnabled = false
+        setupMap()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        venues = Venue.all()
-        map.addAnnotations(venues)
-        map.showAnnotations(venues, animated: true)
+
+        reloadAnnotations()
     }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -44,5 +41,51 @@ class ListVenuesViewController: ShowtimeBaseViewController, MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let venue = view.annotation as? Venue else { return }
         didSelect(venue)
+    }
+
+    // MARK: Private section
+
+    func setupMap() {
+        map.delegate = self
+        map.isPitchEnabled = false
+        map.isRotateEnabled = false
+        disableDefaultDoubleTap()
+        defineCustomDoubleTap()
+        zoomExtends()
+    }
+
+    func zoomExtends() {
+        map.showAnnotations(venues, animated: true)
+    }
+
+    func reloadAnnotations() {
+        map.removeAnnotations(map.annotations)
+        venues = Venue.all()
+        map.addAnnotations(venues)
+    }
+
+    func disableDefaultDoubleTap() {
+        // Apple's default gestures are added to a map subiew, 
+        // traverse them to remove the default double tap behaviour.
+        for view in map.subviews {
+            for gesture in view.gestureRecognizers ?? [] {
+                guard let tapGesture = gesture as? UITapGestureRecognizer else { return }
+
+                if tapGesture.numberOfTapsRequired == 2 {
+                    map.subviews[0].removeGestureRecognizer(tapGesture)
+                }
+            }
+        }
+    }
+
+    func defineCustomDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(mapDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        map.addGestureRecognizer(doubleTap)
+    }
+
+    func mapDoubleTapped() {
+        zoomExtends()
     }
 }
